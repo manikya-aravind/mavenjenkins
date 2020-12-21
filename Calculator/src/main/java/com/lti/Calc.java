a0c936574772ae2d71e7902aced2649770b72470
manikya-aravind <manikyaaravind@gmail.com>
2020-12-21 15:10:42 +0530

    modified

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -9,11 +9,16 @@ public class Calc {
 	public static int subs(int num1,int num2){
 		return num1-num2;
 	}
+	
+	public static int prod(int num1,int num2){
+		return num1*num2;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(add(10,20));
 		System.out.println(subs(20,10));
+		System.out.println(prod(20,10));
 
 	}
 
