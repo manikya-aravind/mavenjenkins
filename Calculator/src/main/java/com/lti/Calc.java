260f073d708e2f2e8547a5c984b517f2a4a8bc0f
manikya-aravind <manikyaaravind@gmail.com>
2020-12-21 14:59:28 +0530

    modified

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -5,10 +5,15 @@ public class Calc {
 	public static int add(int num1,int num2){
 		return num1+num2;
 	}
+	
+	public static int subs(int num1,int num2){
+		return num1-num2;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(add(10,20));
+		System.out.println(subs(20,10));
 
 	}
 
